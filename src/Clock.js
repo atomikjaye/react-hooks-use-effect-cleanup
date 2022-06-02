2af48ec0f55e0a8339c6f5b043cdf9d6f0f8f4f3
Jocelyn Naarden <tzionone@gmail.com>
2022-06-02 16:18:16 -0400

    Done

--- src/Clock.js
+++ src/Clock.js
@@ -4,9 +4,15 @@ function Clock() {
   const [time, setTime] = useState(new Date());
 
   useEffect(() => {
-    setInterval(() => {
+    const timerID = setInterval(() => {
       setTime(new Date());
     }, 1000);
+    return function cleanup() {
+      // the works because we change state when we toggle the clock, causing
+      // the function to be run again
+      console.log("cleaned")
+      clearInterval(timerID)
+    }
   }, []);
 
   return <div>{time.toString()}</div>;
